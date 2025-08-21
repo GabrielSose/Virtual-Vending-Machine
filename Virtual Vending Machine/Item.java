604080089e993705230c76825997976dbd79728e
GabrielSose <72208832+GabrielSose@users.noreply.github.com>
2025-08-20 23:45:11 -0500

    Update Item.java

--- Virtual Vending Machine/Item.java
+++ Virtual Vending Machine/Item.java
@@ -1,1 +1,34 @@
+public class Item {
+    private String name;
+    private double price;
+    private int quantity;
 
+    public Item(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void reduceQuantity() {
+        if (quantity > 0) {
+            quantity--;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + " - $" + price + " (" + quantity + " left)";
+    }
+}
